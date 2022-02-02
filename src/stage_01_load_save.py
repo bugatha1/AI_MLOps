e544e684abe11cae6334e8414f32414882fbea5e
bugatha1 <umasankar.bugatha@outlook.com>
2022-02-02 08:43:00 -0600

    added stage_01

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -2,24 +2,28 @@ from src.utils.all_utils import read_config, create_directory
 import argparse
 import pandas as pd
 import os
+import shutil
+from tqdm import tqdm
+
+def copy_file(source_download_dir, local_data_dir):
+    list_of_files = os.listdir(source_download_dir)
+    N = len(list_of_files)
+    for file in tqdm(list_of_files, total=N, desc=f" copying file from {source_download_dir} to {local_data_dir}",
+    color="green" ):
+        src = os.path.join(source_download_dir, file)
+        dest = os.path.join(local_data_dir, file)
+        shutil.copy(src, dest)
 
 def get_data(config_path):
     config = read_config(config_path)
     
-    remote_data_path = config["data_source"]
-    df = pd.read_csv(remote_data_path, sep=";")
-    
-    artifacts_dir = config["artifacts"]["artifacts_dir"]
-    raw_local_dir = config["artifacts"]["raw_local_dir"]
-    raw_local_file = config["artifacts"]["raw_local_file"]
+    source_download_dirs = config["source_download_dirs"]
+    local_data_dirs = config["local_data_dirs"]
 
-    raw_local_dir_path = os.path.join(artifacts_dir, raw_local_dir)
-
-    create_directory(dirs = [raw_local_dir_path])
-
-    raw_local_file_path = os.path.join(raw_local_dir_path, raw_local_file)
-
-    df.to_csv(raw_local_file_path, sep=",", index=False)
+    for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs, local_data_dirs), total=2, desc="iterate folders", 
+    color="red"):
+        create_directory([local_data_dir])
+        copy_file(source_download_dir, local_data_dir)
 
 
 if __name__ == "__main__":
