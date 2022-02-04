f74637e9d7f5e56de361a2e60a339ec8f27f0dda
bugatha1 <umasankar.bugatha@outlook.com>
2022-02-04 16:12:25 -0600

    stage 03 completed

--- src/stage_03_prepare_callbacks.py
+++ src/stage_03_prepare_callbacks.py
@@ -1,5 +1,4 @@
 from src.utils.all_utils import rea read_config, create_directory
-from src.utils.models import get_VGG_16_model, prepare_model
 from src.utils.callbacks import create_and_save_tensorboard_callback, create_and_save_checkpoint_callback
 import argparse
 import pandas as pd
@@ -7,7 +6,6 @@ import os
 import shutil
 from tqdm import tqdm
 import logging
-import io
 
 logging_str = "[%(asctime)s : %(levelname)s: %(module)s] : %(message)s"
 logging_dir = "logs"
